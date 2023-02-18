61ec129123a60fce258892440842a379a5a98195
Duygu Algan <duygu.algan.esk26@gmail.com>
2023-02-18 20:05:03 +0300

    edit blackJackGame

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,30 +1,53 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
 
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard,secondCard]
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let sum = firstCard + secondCard
+ 
+let cards = []
+let sum = 0
 let hasBlackJack = false
 //console.log(sum)
-let isAlive = true
+let isAlive = false 
 let message = ""
 let sumEl = document.getElementById("sum-el")
-
+let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 
-let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+let player = {
+     name: "Duygu",
+     chips: 45
+}
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $ "+ player.chips
 
+
+
+function getRandomCard (){
+    let randomNumber =  Math.floor( Math.random() * 13) +1 
+    if(randomNumber > 10){
+        return 10
+    }else if(randomNumber === 1){
+        return 11
+    }else{
+        return randomNumber
+    }
+
+}
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard , secondCard]
+    sum = firstCard+ secondCard
     renderGame()
 }
 
 function renderGame(){
-    cardsEl.textContent = "Cards: " +cards[0] +" " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for( let i = 0; i<cards.length; i++){
+        cardsEl.textContent+= cards[i] +" "
+    }
+
     sumEl.textContent = "sum: " + sum
     if( sum <= 20){
         message = "do you want to draw a new card?"
@@ -40,15 +63,18 @@ function renderGame(){
     messageEl.textContent  = message
 }
 function newCard(){
-     let card = 7
+     if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
      sum += card
+     cards.push(card)
      renderGame()
+     }
 }
 
 
 
 // console.log(isAlive)
 // console.log(hasBlackJack)
-console.log(message)
+// console.log(message)
 
 //*******************************/*//////
--- BlackJack Game/index2.js
+++ BlackJack Game/index2.js
@@ -30,3 +30,96 @@
 // }else{
 //     console.log("notelegible , you have already gotten one")
 // }
+
+
+
+///*********************//************************* */ */
+let cardss = [7,4]
+cardss.push(6)
+console.log(cardss)
+cardss.pop()
+console.log(cardss)
+
+//*************************************** */
+
+// count to ten
+
+// we neeed to specify
+
+// where shoul we start CountQueuingStrategywhere is the finish line 
+// what is the step size we should use
+
+//     start        finish       step size
+for ( let count = 1; count < 11; count +=1 ){
+    console.log(count)
+}
+
+let sentence = ["hello", "my", "name", "is", "duygu"]
+let greetingEl= document.getElementById("greeting-el")
+
+
+for(let i = 0; i< sentence.length; i++){
+    greetingEl.textContent += sentence[i]+ " "
+}
+
+
+//*********************** */
+
+let player1time = 102
+let player2time = 107
+function getFastestRaceTime(){
+    if(player1time<player2time){
+        return player1time
+    }else if (player2time< player1time){
+        return player2time
+    }else {
+        return player1time
+    }
+}
+
+// let fasetestRace = getFastestRaceTime()
+// console.log(fasetestRace)
+
+function getTotalRaceTime() {
+    return player1time + player2time
+}
+let totaltime= getTotalRaceTime()
+console.log(totaltime)
+
+
+//******************** */
+// random number generater
+
+let rondomNumber = Math.random() * 6 
+console.log(rondomNumber)
+// 0 ile 1 arasında random sayı üretir 1 dahil değil
+
+let flooredNumber = Math.floor(3.45632)
+
+// it removes the desimal
+
+let randomNumbers = Math.floor( Math.random() * 6) +1 
+
+function rollDice(){
+    let randomNumbers = Math.floor( Math.random() * 6) +1 
+    return randomNumbers
+}
+
+
+
+///////***************************************** */
+///logical operation
+
+let hasCompletedCourse = true
+let givesCertificate = true
+
+if ( hasCompletedCourse === true && givesCertificate === true){
+    // if(givesCertificate === true){    ||  or operation
+    //     generateCertificate()
+    // }
+    generateCertificate()
+}
+
+function generateCertificate(){
+    console.log("generating certificate ......")
+}
